a3fb9dca27a5c8a0960606fe3ad2628fc922f742
azzz9 <182873656+azzz9@users.noreply.github.com>
2026-03-29 15:26:56 +0000

    fix(nvim): migrate treesitter setup to new API

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -85,7 +85,6 @@ in
         nvim-dap-virtual-text
         nvim-dap-python
         nvim-nio
-        nvim-treesitter
         nvim-treesitter-textobjects
         indent-blankline-nvim
         rainbow-delimiters-nvim
@@ -227,20 +226,27 @@ in
       local parser_install_dir = vim.fn.stdpath("data") .. "/treesitter"
       vim.fn.mkdir(parser_install_dir, "p")
       vim.opt.runtimepath:append(parser_install_dir)
-      require("nvim-treesitter.config").setup({
-        highlight = { enable = true },
-        indent = { enable = true },
-        rainbow = { enable = true, extended_mode = true },
-        auto_install = false,
-        parser_install_dir = parser_install_dir,
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-            },
+      require("nvim-treesitter").setup({
+        install_dir = parser_install_dir,
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          if vim.bo[args.buf].buftype ~= "" then
+            return
+          end
+          local has_parser = pcall(vim.treesitter.get_parser, args.buf)
+          if has_parser then
+            pcall(vim.treesitter.start, args.buf)
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+      require("nvim-treesitter-textobjects").setup({
+        select = {
+          lookahead = true,
+          keymaps = {
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
           },
         },
       })
