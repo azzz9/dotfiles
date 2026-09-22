c441be64d1b2b4c4351f9f724d5e20118f017817
azzz9 <182873656+azzz9@users.noreply.github.com>
2026-09-22 23:38:37 +0900

    feat(nvim): reload oil buffers on external filesystem changes

--- modules/nvim/lua/plugins/oil.lua
+++ modules/nvim/lua/plugins/oil.lua
@@ -1,5 +1,6 @@
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
       require("oil").setup({
+        watch_for_changes = true,
         keymaps = {
           ["g."] = "actions.toggle_hidden",
         },
